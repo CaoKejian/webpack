8124b45c3dc52a31e4119a8387bccd17bac98fa1
曹珂俭 <1849201815@qq.com>
2023-10-07 20:57:35 +0800

    feat: entry 的使用

---
+++ demo_03/webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  mode: 'production',
+  // entry: ["./src/test1.js", "./src/test2.js"]
+  // entry: {
+  //   test1: './src/test1.js',
+  //   test2: './src/test2.js',
+  // }
+
+  // entry: {
+  //   t1: './src/test1.js',
+  //   t2: {
+  //     dependOn: 't1',
+  //     import: './src/test2.js',
+  //     runtime: 'x2'
+  //   }
+  // }
+
+  // entry: {
+  //   t1: './src/test1.js',
+  //   t2: {
+  //     import: './src/test2.js',
+  //     runtime: 't1'
+  //   }
+  // }
+
+  // entry: {
+  //   t1: {
+  //     import: './src/test1.js',
+  //     dependOn: 't2'
+  //   },
+  //   t2: {
+  //     import: './src/test2.js',
+  //     dependOn: 't1'
+  //   }
+  // }
+}
---
+++ demo_03/x/x_test.js
@@ -0,0 +1,1 @@
+console.log('Hi, x_test')
