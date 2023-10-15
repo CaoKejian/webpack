663f1ca2151b5ead94e99e6d863b9a5d8028a4f7
曹珂俭 <1849201815@qq.com>
2023-10-15 13:59:28 +0800

    feat: loader的使用

---
+++ demo_03/src/index.js
@@ -0,0 +1,3 @@
+ import './style/indes.css'
+ 
+ document.body.insertAdjacentHTML('beforeend', '<h1>Hi,i am index.js</h1>')
--- demo_03/webpack.config.js
+++ demo_03/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
   mode: 'production',
   // entry: ["./src/test1.js", "./src/test2.js"]
@@ -33,4 +34,37 @@ module.exports = {
   //     dependOn: 't1'
   //   }
   // }
+
+  //  entry introdcuct
+  // entry: './src/test1.js',
+  // output: {
+  //   filename: 'colin1.js',
+  //   clean: true,
+  //   path: path.resolve(__dirname, 'colin_package')
+  // }
+
+  // 报错 Multiple chunks emit assets to the same filename colin.js 
+  // entry: {
+  //   test1: './src/test1.js',
+  //   test2: './src/test2.js'
+  // },
+  // output: {
+  //   // filename: 'colin.js',
+  //   // filename: '[name].js',
+  //   filename: '[name]-[id]-[hash].js',
+  //   clean: true,
+  //   path: path.resolve(__dirname, 'colin_package')
+  // }
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpeg|jpg|png)/,
+        type: 'asset/resource'
+      }
+    ]
+  }
 }
